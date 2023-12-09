2f69a7fca22a184ed6baea0699567ec36c3632d2
DayLayKuny <haykkazaryan2007@gmail.com>
2023-12-09 09:19:49 -0800

    Updates

--- src/components/loading/nothing/console.jsx
+++ src/components/loading/nothing/console.jsx
@@ -1,49 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import "./console.css"
+import React from "react";
+import "./Console.css"
 
-const GreetingComponent = () => {
-  const [greeting, setGreeting] = useState([
-    'Hello. I am a console wannabe.',
-    'All systems are functioning.',
-    'I like pie.'
-  ]);
-  const [text, setText] = useState([]);
-  const [currentLine, setCurrentLine] = useState(0);
+function Console () {
+    return (
+        // <div className="all-console">
+        //     <div className="top">
+        //         <p>cmd_{'<'}</p>
+        //     </div>
+            <div className="consoleMain">
+                <div id="cns" class="cns">
+                        <div className="style1"></div>
+                        <span id="cnss" class="cnss">
+                            <span class="teraam">server@kali:~$</span>&nbsp;
+                            <span class="teraame" id="1cnsvi">cd hackTool</span><br />
+                            <span class="teraam">server@kali:~/hackTool$</span>&nbsp;
+                            <span class="teraame" id="3cnsvi">bruteForce</span><br />
+                            <span class="teraam">server@kali:~/bruteForce$</span>&nbsp;
+                            <span class="teraame" id="5cnsvi">bash gazan.sh</span><br />
+                            <span class="teraame" id="7cnsvi">. . . . . . . .</span><br />┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈<br />
+                            &nbsp;ETR: <span id="etranul">&nbsp;</span><span id="etra">0</span><span id="etraplu"></span>
+                            <span id="etramse">sec</span> : <span id="etrspeine">7.46</span>
+                             gB/sec<br />&nbsp;<span id="cnsscennul"></span><span id="cnsscena">100%</span>
+                            <span id="cnsscenb"><span id="cnssproga"><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
+                            <i class="fas fa-rectangle-portrait"></i></span><span id="cnssprogb"></span></span><br />
+                            &nbsp;<span class="teraamf">&nbsp;Select Type&nbsp;</span><br />&nbsp;<span class="teraamd">
+                            <span class="teraamg">&nbsp;<i class="far fa-angle-right"></i>&nbsp;</span>web<span class="teraamg">
+                            &nbsp;/&nbsp;</span>app<span class="teraamg">&nbsp;/&nbsp;</span>xss<span class="teraamg">&nbsp;/&nbsp;
+                            </span>gps<span class="teraamg">&nbsp;/&nbsp;</span>sql<span class="teraamg">&nbsp;<i class="far fa-angle-down">
+                            </i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;
+                            <span class="teraamc" id="15cnsvi">web</span><br />&nbsp;<span class="teraamf">&nbsp;Select Subtype&nbsp;</span>
+                            <br />&nbsp;<span class="teraamd"><span class="teraamg">&nbsp;<i class="far fa-angle-right"></i>&nbsp;</span>www
+                            <span class="teraamg">&nbsp;/&nbsp;</span>social<span class="teraamg">&nbsp;/&nbsp;</span>messenger
+                            <span class="teraamg">&nbsp;/&nbsp;</span>server<span class="teraamg">&nbsp;<i class="far fa-angle-down"></i>&nbsp;</span></span><br />
+                            <span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;<span class="teraamc" id="19cnsvi">social</span><br />
+                            &nbsp;<span class="teraamf">&nbsp;Social Network&nbsp;</span><br />&nbsp;<span class="teraamd">
+                                <span class="teraamg">&nbsp;<i class="far fa-angle-right"></i>&nbsp;</span>vkontakte<span class="teraamg">&nbsp;/&nbsp;</span>
+                                odnoklassniki<span class="teraamg">&nbsp;/&nbsp;</span>instagram<span class="teraamg">&nbsp;/&nbsp;</span>
+                                tiktok<span class="teraamg">&nbsp;/&nbsp;</span>whatsapp<span class="teraamg">&nbsp;<i class="far fa-angle-down">
+                                    </i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;
+                                    <span class="teraamc" id="23cnsvi">instagram</span><br />&nbsp;<span class="teraamf">&nbsp;INST Id&nbsp;</span><br />
+                                    &nbsp;<span class="teraamd"><span class="teraamg">&nbsp;Example&nbsp;</span>https://www.instagram.com/user_alex or @user_alex<span class="teraamg">&nbsp;<i class="far fa-angle-down"></i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-chevron-square-right"></i></span>&nbsp;<span class="teraamc" id="27cnsvi">https://www.instagram.com/day.lay_ink</span><br />&nbsp;<span class="teraamf">&nbsp;Continue ?&nbsp;</span><br />&nbsp;<span class="teraamd">&nbsp;Y <span class="teraamg">/</span> n<span class="teraamg">&nbsp;<i class="far fa-angle-down"></i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;<span class="teraamc" id="31cnsvi">Y</span><br /><br /><br />&nbsp;API-Идентификатор:<br />&nbsp;&nbsp;<span class="teraamc" id="33cnsvi">ZzGrLA1TlkdPFaCj99vopROFUsghPwcq</span><br /><span class="cnsloiltera">&nbsp;Логин = [&nbsp;<span id="logbrtd">day.lay_ink</span>&nbsp;]</span><br /><span class="cnsloiltera">&nbsp;Пароль = [&nbsp;<span id="psbrtd">**ylayink-123-***</span>&nbsp;]</span><br />&nbsp;<span id="twteraam_aa" class="twteraam">Подбор доступов...</span><br />&nbsp;<span id="twcnsscennul"></span><span id="twcnsscena">37%</span> <span id="twcnsscenb"><span id="twcnssproga"><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i></span><span id="twcnssprogb"><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i></span></span><br />&nbsp;<span class="teraame" id="35cnsvi"></span><br /></span>
+                </div>
+            </div>
+        // </div>
+    )
+}
 
-  useEffect(() => {
-    const type = () => {
-      const line = greeting[currentLine];
-      if (!line) {
-        setTimeout(() => {
-          setCurrentLine((prevLine) => (prevLine + 1) % greeting.length);
-          setText([]);
-          type();
-        }, 2000);
-        return;
-      }
-
-      const character = line[text.length];
-      if (!character) {
-        setTimeout(type, 300);
-        return;
-      }
-
-      setText((prevText) => [...prevText, character]);
-      setTimeout(type, 300);
-    };
-
-    type();
-  }, [greeting, text, currentLine]);
-
-  return (
-    <div>
-      {text.map((character, index) => (
-        <span key={index}>{character}</span>
-      ))}
-      <br />
-      <i>▮</i>
-    </div>
-  );
-};
-
-export default GreetingComponent;
+export default Console;
