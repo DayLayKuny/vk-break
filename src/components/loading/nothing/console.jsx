b00d8acbf6551ecb3ff0f0b259af02df03166b83
DayLayKuny <haykkazaryan2007@gmail.com>
2023-12-10 02:32:31 -0800

    updates

--- src/components/loading/nothing/console.jsx
+++ src/components/loading/nothing/console.jsx
@@ -1,58 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Console.css"
+export default Console;
 
 function Console () {
+    let empty = []
+    let myShow = ["Host Name:                 USER",
+    ">OS Name:                   Microsoft Windows 8.1 Pro",
+    ">OS Version:                6.3.9600 N/A Build 9600",
+    ">OS Manufacturer:           Microsoft Corporation",
+    "OS Configuration:          Standalone Workstation",
+    "OS Build Type:             Multiprocessor Free",
+    " Registered Owner:          admin<",
+    " Registered Organization:",
+    "> Product ID:                00260-00591-66009-AA910",
+    "> Original Install Date:     11/28/2023, 5:51:11 PM<",
+    ">System Boot Time:          12/10/2023, 12:45:42 AM",
+    "System Manufacturer:       Hewlett-Packard",
+    "System Model:              HP Pavilion g7 Notebook PC",
+    "System Type:               x64-based PC",
+    ">Processor(s):              1 Processor(s) Installed.",
+    ">                        [01]: Intel64 Family 6 Model 58 Stepping 9 GenuineInt",
+    ">el ~1200 Mhz",
+    "BIOS Version:              Insyde F.26, 6/3/2014",
+    "Windows Directory:         C:Windows",
+    "System Directory:          C:Windowssystem32",
+    "Boot Device:               DeviceHarddiskVolume2",
+    "System Locale:             en-us;English (United States)",
+    "Input Locale:              en-us;English (United States)",
+    ">Time Zone:                 (UTC-08:00) Pacific Time (US & Canada)",
+    "Total Physical Memory:     3,988 MB",
+    "Available Physical Memory: 961 MB",
+    "Virtual Memory: Max Size:  6,571 MB",
+    "Virtual Memory: Available: 2,227 MB",
+    ">Virtual Memory: In Use:    4,344 MB<",
+    ">Page File Location(s):     C:pagefile.sys",
+    ">Domain:                    WORKGROUP",
+    "Logon Server:              MicrosoftAccount<",
+    "Hotfix(s):                 170 Hotfix(s) Installed.",
+    "                       [01]: KB2899189_Microsoft-Windows-CameraCodec-Package",
+                            ">Network Card(s):           2 NIC(s) Installed.",
+                            "                       [01]: Realtek PCIe FE Family Controller",
+    "                               Connection Name: Ethernet",
+                            "                               Status:          Media disconnected",
+                                    "                      [02]: Ralink RT3290 802.11bgn Wi-Fi Adapter",
+                                    "                                Connection Name: Wi-Fi",
+                            "                                DHCP Enabled:    Yes",
+                                    "                               DHCP Server:     192.168.1.1<",
+                                    "                                IP address(es)",
+                                    "                               [01]: 192.168.1.12",
+                                    "                                [02]: fe80::94fe:68f6:1de6:669c",
+                                    "                                [03]: 2a02:2a56:1cff::4",
+                                    " Hyper-V Requirements:      VM Monitor Mode Extensions: Yes",
+                                    "                        Virtualization Enabled In Firmware: No",
+    "                        Second Level Address Translation: Yes",
+    "                     Data Execution Prevention Available: Yes" ]
+    const justShow = () => {
+        for (let i = 0; i < myShow.length; i++) {
+            setTimeout(() => {
+                (empty[i] = myShow[i]);
+            }, i * 1000);
+        }
+    };
+
+  
+
     return (
-        // <div className="all-console">
-        //     <div className="top">
-        //         <p>cmd_{'<'}</p>
-        //     </div>
-            <div className="consoleMain">
-                <div id="cns" class="cns">
-                        <div className="style1"></div>
-                        <span id="cnss" class="cnss">
-                            <span class="teraam">server@kali:~$</span>&nbsp;
-                            <span class="teraame" id="1cnsvi">cd hackTool</span><br />
-                            <span class="teraam">server@kali:~/hackTool$</span>&nbsp;
-                            <span class="teraame" id="3cnsvi">bruteForce</span><br />
-                            <span class="teraam">server@kali:~/bruteForce$</span>&nbsp;
-                            <span class="teraame" id="5cnsvi">bash gazan.sh</span><br />
-                            <span class="teraame" id="7cnsvi">. . . . . . . .</span><br />┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈<br />
-                            &nbsp;ETR: <span id="etranul">&nbsp;</span><span id="etra">0</span><span id="etraplu"></span>
-                            <span id="etramse">sec</span> : <span id="etrspeine">7.46</span>
-                             gB/sec<br />&nbsp;<span id="cnsscennul"></span><span id="cnsscena">100%</span>
-                            <span id="cnsscenb"><span id="cnssproga"><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i>
-                            <i class="fas fa-rectangle-portrait"></i></span><span id="cnssprogb"></span></span><br />
-                            &nbsp;<span class="teraamf">&nbsp;Select Type&nbsp;</span><br />&nbsp;<span class="teraamd">
-                            <span class="teraamg">&nbsp;<i class="far fa-angle-right"></i>&nbsp;</span>web<span class="teraamg">
-                            &nbsp;/&nbsp;</span>app<span class="teraamg">&nbsp;/&nbsp;</span>xss<span class="teraamg">&nbsp;/&nbsp;
-                            </span>gps<span class="teraamg">&nbsp;/&nbsp;</span>sql<span class="teraamg">&nbsp;<i class="far fa-angle-down">
-                            </i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;
-                            <span class="teraamc" id="15cnsvi">web</span><br />&nbsp;<span class="teraamf">&nbsp;Select Subtype&nbsp;</span>
-                            <br />&nbsp;<span class="teraamd"><span class="teraamg">&nbsp;<i class="far fa-angle-right"></i>&nbsp;</span>www
-                            <span class="teraamg">&nbsp;/&nbsp;</span>social<span class="teraamg">&nbsp;/&nbsp;</span>messenger
-                            <span class="teraamg">&nbsp;/&nbsp;</span>server<span class="teraamg">&nbsp;<i class="far fa-angle-down"></i>&nbsp;</span></span><br />
-                            <span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;<span class="teraamc" id="19cnsvi">social</span><br />
-                            &nbsp;<span class="teraamf">&nbsp;Social Network&nbsp;</span><br />&nbsp;<span class="teraamd">
-                                <span class="teraamg">&nbsp;<i class="far fa-angle-right"></i>&nbsp;</span>vkontakte<span class="teraamg">&nbsp;/&nbsp;</span>
-                                odnoklassniki<span class="teraamg">&nbsp;/&nbsp;</span>instagram<span class="teraamg">&nbsp;/&nbsp;</span>
-                                tiktok<span class="teraamg">&nbsp;/&nbsp;</span>whatsapp<span class="teraamg">&nbsp;<i class="far fa-angle-down">
-                                    </i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;
-                                    <span class="teraamc" id="23cnsvi">instagram</span><br />&nbsp;<span class="teraamf">&nbsp;INST Id&nbsp;</span><br />
-                                    &nbsp;<span class="teraamd"><span class="teraamg">&nbsp;Example&nbsp;</span>https://www.instagram.com/user_alex or @user_alex<span class="teraamg">&nbsp;<i class="far fa-angle-down"></i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-chevron-square-right"></i></span>&nbsp;<span class="teraamc" id="27cnsvi">https://www.instagram.com/day.lay_ink</span><br />&nbsp;<span class="teraamf">&nbsp;Continue ?&nbsp;</span><br />&nbsp;<span class="teraamd">&nbsp;Y <span class="teraamg">/</span> n<span class="teraamg">&nbsp;<i class="far fa-angle-down"></i>&nbsp;</span></span><br /><span class="teraamb">&nbsp;<i class="fal fa-question-square"></i></span>&nbsp;<span class="teraamc" id="31cnsvi">Y</span><br /><br /><br />&nbsp;API-Идентификатор:<br />&nbsp;&nbsp;<span class="teraamc" id="33cnsvi">ZzGrLA1TlkdPFaCj99vopROFUsghPwcq</span><br /><span class="cnsloiltera">&nbsp;Логин = [&nbsp;<span id="logbrtd">day.lay_ink</span>&nbsp;]</span><br /><span class="cnsloiltera">&nbsp;Пароль = [&nbsp;<span id="psbrtd">**ylayink-123-***</span>&nbsp;]</span><br />&nbsp;<span id="twteraam_aa" class="twteraam">Подбор доступов...</span><br />&nbsp;<span id="twcnsscennul"></span><span id="twcnsscena">37%</span> <span id="twcnsscenb"><span id="twcnssproga"><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i><i class="fas fa-rectangle-portrait"></i></span><span id="twcnssprogb"><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i><i class="far fa-rectangle-portrait"></i></span></span><br />&nbsp;<span class="teraame" id="35cnsvi"></span><br /></span>
-                </div>
+        <div className="all-console">
+            <div className="top">
+                <p>cmd_{'<'}</p>
             </div>
-        // </div>
+            <div className="consoleMain">
+                <pre>
+                    {
+                        myShow.map(el => {
+                            setTi
+                            return (
+                                <p>{el}</p>
+                            )
+                        })
+                    }
+                </pre>
+            </div>
+        </div>
     )
 }
-
-export default Console;
